b38cd024e569bd583acabdd7cca227db9db0ec63
Nodeclient <leo.zacharry@gmail.com>
2020-10-14 22:26:35 +0300

    update test.js file

--- sample_upload_project/test.js
+++ sample_upload_project/test.js
@@ -4,19 +4,16 @@ const ld =  require('largedata').default;
 const app = express();
 
 	const option = { 
-		encoding:'binary',       // file encode type
-		request_size:"1gb",      // for each blob "each part"
-		storage:"./upload",      // your file is saved in this folder
-		parameter_limit:"10000", 
-		overwrite: true ,
-		 mime_types : [ ]        // example : "application/javascript", "application/json", "application/zip"
+		 encoding:'binary',       // encode type
+		 storage:"./upload",      // your storage folder
+		 overwrite: false ,       // file overwrite
+		 mime_types : [ ]         // upload limit option
 	}
 	
   app.set('views', path.join(process.cwd(),'views') )
   app.set('view engine', 'ejs');
-  app.use(express.urlencoded({ limit: option.request_size , extended: true }))
-  app.use(express.json({ limit: option.request_size  }));
-  
+  app.use(express.urlencoded({ extended: true }))
+
   
   app.get('/',function (req, res, next) {
     res.send("<center> let's go > <a href=/upload>upload test</a></center>")
@@ -33,13 +30,15 @@ const app = express();
     }
     if(fields){ // HTML input elements (excepting "input file element")
       for (const items of fields) {
+        //SEND TEST MESSAGE TO CLIENT
+          client.post({
+            hello: "Good Morning",
+            language_test: "testing some different languages :> Günaydın, доброе утро, शुभ प्रभात ,добрий ранок, おはようございます, 早上好, buổi sáng tốt lành"
+          }); 
+        //-----------------------------
         console.log( items );
       }
 	  
-		client.post({
-			hello: "Good Morning",
-			language_test: "testing some different languages :> Günaydın, доброе утро, शुभ प्रभात ,добрий ранок, おはようございます, 早上好, buổi sáng tốt lành"
-		}); // send data-object from server to client
 
     }
     
